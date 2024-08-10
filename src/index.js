e9fafd1a65a111d9a369a1142f0cf62c947a9de8
Yash Verma <yashv@lambdatest.com>
2024-08-10 15:07:39 +0530

    chatapp backend

---
+++ src/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const WebSocket = require('ws');
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
+
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  // bootstrap(/*{ strapi }*/) {},
+
+
+  bootstrap({ strapi }) {
+    // Start the WebSocket server
+    const wss = new WebSocket.Server({ port: 8080 });
+
+    wss.on('connection', (ws) => {
+      ws.on('message', (message) => {
+        console.log('Received:', message);
+        // Echo the message back to the client
+        ws.send(`Server: ${message}`);
+      });
+
+      ws.send('Welcome to Aynas`s World!');
+    });
+
+    strapi.wss = wss; // Attaching the WebSocket server to the Strapi instance
+    console.log('WebSocket server is running on ws://localhost:8080');
+  },
+};
